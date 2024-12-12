6bade0706d2a0d80f1e0530f17b3180403f6da36
Pushkar021 <157273057+Pushkar021@users.noreply.github.com>
2024-12-12 18:25:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,63 @@ import toast, { Toaster } from "react-hot-toast";
 function App() {
   const [getval, setval] = useState("");
   const [getop, setop] = useState("");
+  const [getleft, setleft] = useState("Char");
+  const [getright, setright] = useState("Binary");
 
   const handleclick = () => {
     main(getval);
   };
 
   const notify = () => {
+    
     if (getop) {
-      navigator.clipboard.writeText(getop); 
-      toast.success("Copied to clipboard!"); 
-    } else {
-      toast.error("No binary output to copy!"); 
+         navigator.clipboard.writeText(getop);
+         toast.success("Copied to clipboard!");
+       }
+     else { toast.error("No binary output to copy!");
     }
   };
 
   function main(text) {
-    let x = text
-      .split("")
-      .map((char) => char.charCodeAt(0).toString(2))
-      .join(" ");
-    setop(x);
+     console.log(getleft);
+    console.log(getright);
+     if (getleft == getright) {
+      toast.error("Can not Convert! ");
+     }
+     else{
+    if(getleft=="Char"){
+      let x = text.split("").map((char)=>char.charCodeAt(0).toString(2)).join(" ")
+      setop(x)
+    } 
+    else if(getleft=="Binary"){
+      let x = text
+        .trim()
+        .split(" ")
+        .map((bin) => String.fromCharCode(parseInt(bin, 2))).join("");  
+        setop(x)
+        
+    }
+    
+    }
+    
   }
 
   return (
     <>
       <div className="main-container">
-        <Toaster /> 
+        <Toaster />
         <h1>Binary Converter</h1>
+        <div>
+          <select onChange={(e) => setleft(e.target.value)}>
+            <option selected>Char</option>
+            <option>Binary</option>
+          </select>
+          to
+          <select onChange={(e) => setright(e.target.value)}>
+            <option selected>Binary</option>
+            <option>Char</option>
+          </select>
+        </div>
         <div>
           <input
             type="text"
@@ -42,9 +72,17 @@ function App() {
         <div>
           <textarea value={getop} readOnly />
         </div>
-          <button onClick={notify}>Copy</button>
+        <button onClick={notify}>Copy</button>
         <div>
           <button onClick={handleclick}>Convert</button>
+        </div>
+        <div class="github">
+          <a
+            href="https://github.com/Pushkar021/Binary-Converter"
+            target="_blank"
+          >
+            <i class="fab fa-github"></i>
+          </a>
         </div>
       </div>
     </>
